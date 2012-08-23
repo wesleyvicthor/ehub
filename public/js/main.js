4e5bb27dccf926a5b57de175406e564cd81bdadb
Wesley Victhor Mendes <w.v.mendes.s@gmail.com>
2012-08-23 12:18:22 -0300

    added js

---
+++ public/js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+	var url = 'tweets.json';
+
+	$('#schedule-info').popover({
+		trigger: 'hover',
+		placement: 'right'
+	});
+	$('#date-one').popover({
+		trigger: 'hover',
+		placement: 'right'
+	});
+	$('#date-two').popover({
+		trigger: 'hover',
+		placement: 'right'
+	});
+	$('#the-venue').popover({
+		trigger: 'hover',
+		placement: 'right'
+	});
+	$('#your-judges').popover({
+		trigger: 'hover',
+		placement: 'right'
+	});
+	$('#the-map').popover({
+		trigger: 'hover',
+		placement: 'right'
+	});
+	$('#apis-list').popover({
+		trigger: 'hover',
+		placement: 'right'
+	});
+	$('#recommended').popover({
+		trigger: 'hover',
+		placement: 'right'
+	});
+
+	setInterval(function(){
+		$.get(url, function(result) {
+			if (!result.length) {
+				return;
+			}
+			$twitt = $('.twitt');
+			$('.twitt ul').empty();
+			$.each(result, function (i, el) {
+				var stripped = (i % 2 == 0)? 'odd' : 'even';
+				var $list = $('<li class="list-twitt '+stripped+'"><div class="img-wrapper"><img width="20" src="'+el.profile+'" /></div><div style="line-height: 11px">'+el.text+'</div><div>'+el.user+'</div></li>');
+				$('.twitt ul').prepend($list);
+			});
+
+		}, 'json');
+	}, 500);
+
+});
